d7ea3db7857ca3ecc9b92646ac85400957bb6cb4
mihaisvfr <mihbox@bk.ru>
2023-09-22 15:22:33 +0300

    sepsrate func

--- api/handlers/quote.py
+++ api/handlers/quote.py
@@ -3,31 +3,29 @@ from api.models.author import AuthorModel
 from api.models.quote import QuoteModel
 
 
-@app.route('/quotes', methods=["GET"])
 @app.route('/quotes/<int:quote_id>', methods=["GET"])
-@app.route('/authors/<int:author_id>/quotes', methods=["GET"])
-def get_quotes(author_id=None, quote_id=None):
-    """
-    Обрабатываем GET запросы
-    :param author_id: id автора
-    :param quote_id: id цитаты
-    :return: http-response(json, статус)
-    """
-    print(f"{author_id=} {quote_id=}")
-    if author_id is None and quote_id is None:  # Если запрос приходит по url: /quotes
-        quotes = QuoteModel.query.all()
-        return [quote.to_dict() for quote in quotes]  # Возвращаем ВСЕ цитаты
-
-    if author_id:  # Если запрос приходит по url: /authors/<int:author_id>/quotes
-        author = AuthorModel.query.get(author_id)
-        quotes = author.quotes.all()
-        return [quote.to_dict() for quote in quotes], 200  # Возвращаем все цитаты автора
-
-    # Если запрос приходит по url: /quotes/<int:quote_id>
+def get_quotes(quote_id):
     quote = QuoteModel.query.get(quote_id)
     if quote is not None:
         return quote.to_dict(), 200
     return {"Error": "Quote not found"}, 404
+
+
+@app.route('/quotes', methods=["GET"])
+def all_quotes():
+    quotes = QuoteModel.query.all()
+    if quotes:
+        return [quote.to_dict() for quote in quotes]
+    return {"Error": "Quotes not found"}, 404
+
+
+@app.route('/authors/<int:author_id>/quotes', methods=["GET"])
+def quote_by_author(author_id):
+    author = AuthorModel.query.get(author_id)
+    quotes = author.quotes.all()
+    if author is not None:
+        return [quote.to_dict() for quote in quotes], 200
+    return {"Error": f"Author id={author_id} not found"}, 404
 
 
 @app.route('/authors/<int:author_id>/quotes', methods=["POST"])
