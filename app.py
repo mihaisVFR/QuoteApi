06f3512d6a02b0706875371f68c030016c4a2a6c
mihaisvfr <mihbox@bk.ru>
2023-09-28 15:14:54 +0300

    finish 12 video

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from api.handlers import author
 from api.handlers import quote
 from api.models.model_homework import Learner
 from api.handlers import user
+from api.handlers import token
 
 if __name__ == '__main__':
     app.run(debug=Config.DEBUG, port=Config.PORT)
