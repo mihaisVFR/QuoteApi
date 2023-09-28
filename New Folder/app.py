0a1da1bceb30d0a9c0c23c1bfcf91b023eceddd8
mihaisvfr <mihbox@bk.ru>
2023-09-28 15:02:07 +0300

    del folder

--- New Folder/app.py
+++
@@ -1,9 +0,0 @@
-from api import app
-from config import Config
-from api.handlers import author
-from api.handlers import quote
-from api.models.model_homework import Learner
-from api.handlers import user
-
-if __name__ == '__main__':
-    app.run(debug=Config.DEBUG, port=Config.PORT)
--- New Folder/tmp/schema.py
+++
@@ -1,7 +0,0 @@
-from marshmallow import Schema, fields
-
-
-class AuthorSchema(Schema):
-    id = fields.Integer()
-    name = fields.Str(required=True, error_messages={"messege": "Error name is required", "code": 400})
-    email = fields.Email(required=True, error_messages={"required": "Error email is required"})
